1804e0a12ac7e6b3de6cf5c3ed11dadb51e2afe4
ludvig <ludvigmalm@gmail.com>
2024-05-31 00:28:44 +0200

    Tog bort .vs fil, alla obj och debug-mappar från båda projekt, för att få bort lokala referenser

--- xUnitTestProject/obj/Debug/net8.0/xUnitTestProject.AssemblyInfo.cs
+++ xUnitTestProject/obj/Debug/net8.0/xUnitTestProject.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("xUnitTestProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e600b6b892c823adf04c9236cc97f2278fe5dc19")]
 [assembly: System.Reflection.AssemblyProductAttribute("xUnitTestProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("xUnitTestProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
